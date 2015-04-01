b0f71ec77acca65c9806ec55de64ee53c249c714
Andrew Dolgert <adolgert@cornell.edu>
2015-03-31 22:18:34 -0400

    Switched random number generator from Boost to STD so that it works with clang++ on the mac. Changed configure.ac to admit that the only c++ it will use is the one that R uses.

--- hopskip/src/rng.hpp
+++ hopskip/src/rng.hpp
@@ -1,10 +1,11 @@
 #ifndef _RNG_HPP_
 #define _RNG_HPP_ 1
 
-#include "boost/random/mersenne_twister.hpp"
+//#include "boost/random/mersenne_twister.hpp"
+#include <random>
 
 namespace hsb {
 // Defines the random number generator for the simulation.
-using RandGen=boost::mt19937;
+using RandGen=std::mt19937;
 }
 #endif
