5c3b3f10e83d8085bc1bf6615e60819c49f3c8a3
Andrew Dolgert <adolgert@cornell.edu>
2015-06-01 10:53:38 -0400

    Made scripts directory. Sizes for landscapes. Seeds set.

--- script/generate_landscape.R
+++ script/generate_landscape.R
@@ -4,6 +4,8 @@ args <- commandArgs(trailingOnly = TRUE)
 house.cnt <- as.integer(args[1])
 block.cnt <- as.integer(args[2])
 save.name <- args[3]
+rng.seed <- as.integer(args[4])
+set.seed(rng.seed)
 landscape.generate(house.cnt, block.cnt, streetsbase=save.name)
 
 
